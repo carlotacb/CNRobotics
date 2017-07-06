32878bb8493a17162b26499ea9d79e022ab9c2ed
Carlota <carkbra@gmail.com>
2017-07-06 13:13:59 +0200

    homee

---
+++ js/login.js
@@ -0,0 +1,9 @@
+/**
+ * Created by carlo on 06/07/2017.
+ */
+
+function myregister(){
+
+   window.open('registre.html', '_blank');
+
+}
---
+++ js/registre.js
@@ -0,0 +1,9 @@
+/**
+ * Created by carlo on 06/07/2017.
+ */
+
+function cancel(){
+
+    window.open('login.html');
+
+}
