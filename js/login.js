3a9899bf4e4fd5b8afc720370bdd8e6b9ba374da
nicodemus73 <n.muscolo@gmail.com>
2017-07-07 16:59:48 +0200

    tot ok

--- js/login.js
+++ js/login.js
@@ -1,9 +1,35 @@
-/**
- * Created by carlo on 06/07/2017.
- */
+var bool;
+function localstg() {
+    var y = document.getElementsByTagName('input');
+    for (var i = 0; i < y.length; i++) {
+        if (y[i].value == "") {
+            alert("El camp " + y[i].name + " es necessari");
+
+            if (y[i].id == 'pass') {
+                document.getElementById('pass').style.borderColor = "red";
+                //return;
+            }
+
+            else if (y[i].id == 'username') {
+                document.getElementById('username').style.borderColor = "red";
+                document.getElementById('pass').value="";
+                //return;
+            }
+
+        }
+    }
+    var a = document.getElementById("username");
+    var b = window.localStorage.getItem("username");
+    var c = document.getElementById("pass");
+    var d = window.localStorage.getItem("password");
+    if ((a.value == b) && (c.value == d)) {
+        bool = true;
+    } else bool = false;
+    if (bool == true) window.open("home.html", '_blank');
+    else  alert("Usuari o contrasenya no vàlids.");
+}
+
 
 function myregister(){
-
-   window.open('registre.html', '_blank');
-
+    window.open('registre.html', '_blank');
 }
--- js/registre.js
+++ js/registre.js
@@ -1,6 +1,40 @@
-/**
- * Created by carlo on 06/07/2017.
- */
+function enregistra() {
+
+    var y = document.getElementsByTagName('input');
+    for (var i = 0; i < y.length; i++) {
+        if (y[i].value == "") {
+            alert("El camp " + y[i].name + " es necessari");
+
+            if (y[i].id == 'pass') {
+                document.getElementById('pass').style.borderColor = "red";
+                //return;
+            }
+            else if (y[i].id == 'pass2') {
+                document.getElementById('pass2').style.borderColor = "red";
+                //return;
+            }
+            else if (y[i].id == 'email') {
+                document.getElementById('email').style.borderColor = "red";
+                //return;
+            }
+            else if (y[i].id == 'username') {
+                document.getElementById('username').style.borderColor = "red";
+                document.getElementById('pass').value="";
+                //return;
+            }
+
+        }
+    }
+    var x = document.getElementById("username");
+
+    var y = document.getElementById("pass2");
+    var z = document.getElementById("pass");
+    if (y.value !== z.value) alert("Contrasenyes diferents");
+    else {
+        window.localStorage.setItem("password", z.value);
+        window.localStorage.setItem("username", x.value);
+    }
+}
 
 function cancel(){
 
